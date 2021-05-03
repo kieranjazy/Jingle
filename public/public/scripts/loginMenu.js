749f35aef5f21b7032fc737c9c9d8e6ccd4f1dbc
AaronBrennan1 <68754265+AaronBrennan1@users.noreply.github.com>
2021-05-03 16:20:03 +0100

    Add files via upload

---
+++ public/public/scripts/loginMenu.js
@@ -0,0 +1,23 @@
+
+function openRegister() {  
+  document.querySelector('.login-box').style.display="none";
+  document.querySelector('.register-box').style.display="block";
+}
+
+function openLogin() {
+  document.querySelector('.login-box').style.display="block";
+  document.querySelector('.register-box').style.display="none";
+  document.querySelector('.resetPassword-box').style.display="none";
+}
+
+function openPasswordRecovery() {
+  document.querySelector('.login-box').style.display="none";
+  document.querySelector('.register-box').style.display="none";
+  document.querySelector('.resetPassword-box').style.display="block";
+}
+ 
+
+
+
+
+
