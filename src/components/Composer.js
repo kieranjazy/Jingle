33383e350759281c3473a45036838285c4942687
Daniel Hannon <hello111f@gmail.com>
2021-05-06 11:44:53 +0100

    sdfdsfsgggrrgr

--- src/components/Composer.js
+++ src/components/Composer.js
@@ -30,18 +30,19 @@ function Composer() {
 	this.audioctx = new AudioContext();
 	// TODO: Implement DrumKits
 	this.loadedInstruments = [
-		{"name":"synth_lead","isLoaded":true,"type":0},
-		{"name":"enya","isLoaded":true,"type":0},
-		{"name":"wobbly","isLoaded":true,"type":0},
-		{"name":"choiraah","isLoaded":true,"type":0},
-		{"name":"TR_909","isLoaded":true,"type":1},
-		{"name":"TR_808","isLoaded":true, "type":1}
+
 	];
 	/*These all follow the form f(x) = (440 * 2^((x-69)/12))/60
 		Where 60 is middle C It's required for the wavetables to work correctly
 		As the Wavetables are all recorded at middle C*/
 	this.keyfreqs = [0.03125, 0.03311, 0.03508, 0.03717, 0.03938, 0.04172, 0.0442, 0.04683, 0.04961, 0.05256, 0.05569, 0.059, 0.06251, 0.06622, 0.07016, 0.07433, 0.07875, 0.08344, 0.0884, 0.09365, 0.09922, 0.10512, 0.11137, 0.118, 0.12501, 0.13245, 0.14032, 0.14867, 0.15751, 0.16687, 0.17679, 0.18731, 0.19844, 0.21024, 0.22275, 0.23599, 0.25002, 0.26489, 0.28064, 0.29733, 0.31501, 0.33374, 0.35359, 0.37461, 0.39689, 0.42049, 0.44549, 0.47198, 0.50005, 0.52978, 0.56129, 0.59466, 0.63002, 0.66749, 0.70718, 0.74923, 0.79378, 0.84098, 0.89099, 0.94397, 1.0001, 1.05957, 1.12257, 1.18932, 1.26004, 1.33497, 1.41435, 1.49845, 1.58756, 1.68196, 1.78197, 1.88793, 2.0002, 2.11913, 2.24514, 2.37865, 2.52009, 2.66994, 2.8287, 2.99691, 3.17511, 3.36391, 3.56394, 3.77587, 4.00039, 4.23827, 4.49029, 4.75729, 5.04018, 5.33988, 5.65741, 5.99381, 6.35022, 6.72783, 7.12789, 7.55173, 8.00078, 8.47653, 8.98057, 9.51459, 10.08035, 10.67976, 11.31481, 11.98763, 12.70045, 13.45566, 14.25577, 15.10346, 16.00156, 16.95307, 17.96115, 19.02917, 20.16071, 21.35952, 22.62963, 23.97526, 25.4009, 26.91131, 28.51155, 30.20693, 32.00313, 33.90613, 35.9223, 38.05835, 40.32141, 42.71905, 45.25926, 47.95051];
 	this.instrumentBank = [];
+	this.instrumentManifests = {{"name":"synth_lead","isLoaded":true,"type":0},
+	{"name":"enya","isLoaded":true,"type":0},
+	{"name":"wobbly","isLoaded":true,"type":0},
+	{"name":"choiraah","isLoaded":true,"type":0},
+	{"name":"TR_909","isLoaded":true,"type":1},
+	{"name":"TR_808","isLoaded":true, "type":1}};
 	this.storedInstruments = [synthLead.audiodata, enya.audiodata, wobbly.audiodata, choiraah.audiodata, tr909.audiodata, tr808.audiodata];
 	this.bps = 1;
 	//Mono by Default for speed
@@ -580,27 +581,27 @@ Composer.prototype.playMetronome = async function () {
 	source.onended = () => { this.playMetronome(); this.metronomeSteps++; };
 };
 
-Composer.prototype.setTrackInstrument = function (trackNo, instrumentData) {
-	if (this.loadedInstruments.length > trackNo) {
-		//this.loadedInstruments.splice(trackNo, 1, instrumentData);
+Composer.prototype.setTrackInstrument = function (trackNo, instrumentPosition) {
+	if (this.loadedInstruments.length > trackNo && trackNo >= 0) {
+		this.loadedInstruments[trackNo] = this.instrumentManifests[instrumentPosition];
 	} else if (this.loadedInstruments.length == trackNo) {
-		//this.loadedInstruments.push(instrumentData);
+		this.loadedInstruments.push(this.instrumentManifests[instrumentPosition]);
+		this.numTracks = this.loadedInstruments.length;
+	} else {
+		return;
 	}
-	this.numTracks = this.loadedInstruments.length;
-	this.fetchInstrument(trackNo, instrumentData);
+	this.fetchInstrument(trackNo, instrumentPosition)
 }
 
 Composer.prototype.fetchInstrument = function (trackNo, instrumentData) {
-	let temp = instrumentData;
-	if(typeof(temp.audiodata) == "undefined") {
-		console.log("Could not add instrument!");
-	}
+	let temp = this.storedInstruments[instrumentData];
 	if(this.instrumentBank.length == trackNo) {
 		//this.instrumentBank.push(Array(temp.audiodata).slice());
-		this.instrumentBank.push(temp)
+		this.instrumentBank.push(temp);
+		this.numTracks = this.instrumentBank.length;
 	} else if(trackNo < this.instrumentBank.length && trackNo >= 0)
 		//this.instrumentBank[trackNo] = Array(temp.audiodata).slice();
-		this.instrumentBank[trackNo] = temp
+		this.instrumentBank[trackNo] = temp;
 };
 
 Composer.prototype.clearTrack = function (trackNo) {
